28f532653bfa35a5df3b93790f71cc37ded501d7
Hyunsuk Lee <blackknight8012@gmail.com>
2017-12-17 21:15:37 +0900

    선택적 tracing 을 위한 nilTracer 와 nilTracer 를 만드는 Off 함수 추가
    아래의 문제 해결 및 사용자 친화를 위한 코드 추가
    
    1. 배포 후 불필요한 log 를 출력하지 않도록 처리
    2. tracer 를 필드로 가지는 type(struct)이
       tracer 를 설정하지 않아서
       코드가 패닉에 빠지는 것을 방지

--- tracer.go
+++ tracer.go
@@ -19,7 +19,18 @@ func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprintln(t.out)
 }
 
-// New is return tracer referance
+// reference followed code
+// https://github.com/golang/go/blob/master/src/io/ioutil/ioutil.go#L110
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// New is create tracer and return
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+// Off is create nilTracer and return
+func Off() Tracer {
+	return &nilTracer{}
 }
